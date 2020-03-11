a50d35f6f2e30766c76e0ee1ea307b060ef81706
Kyle Chesney <kyle.2493@gmail.com>
2020-03-11 15:29:58 -0400

    add gender maps to pickle file add helper script for updating pickle file

---
+++ update_pickle.py
@@ -0,0 +1,35 @@
+import json
+import pickle
+import sys
+
+def update_pickle_file_from_json():
+    try:
+        with open('maps.json') as json_file:
+            mappings = json.load(json_file)
+        with open('oncore_code_mappings.p', 'wb') as pickle_file:
+            pickle.dump(mappings, pickle_file)
+        print('oncore_code_mappings.p updated from maps.json')
+    except:
+        print('You must export your pickle file to json before updating it.')
+
+
+def dump_pickle_to_json():
+    with open('oncore_code_mappings.p', 'rb') as pickle_file:
+        mappings = pickle.load(pickle_file)
+    with open('maps.json', 'w') as json_file:
+        json.dump(mappings, json_file, indent=2)
+    print('Pickle file dumped to maps.json', 'Please make your updates there.\n', 'Once finished, run:', 'python update_pickle.py import', sep='\n')
+
+
+def main():
+    arg = sys.argv[1]
+    if arg == 'export':
+        dump_pickle_to_json()
+    elif arg == 'import':
+        update_pickle_file_from_json()
+    else:
+        print('Valid commands are:\nimport\nexport')
+        
+
+if __name__ == '__main__':
+    main()
