f87c6de2b08faa82c17c601e9e7b7294daa0645a
cshannonUF <cshannon@ufl.edu>
2021-11-08 14:41:35 -0500

    Adding on premise database connection

--- data_provider_service.py
+++ data_provider_service.py
@@ -8,20 +8,29 @@ import os
 logging.basicConfig(filename=os.environ.get('log_file', None), level=logging.DEBUG)
 
 class DataProviderService:
-    def __init__(self, engine):
+    def __init__(self, wallet_engine, op_engine):
         """
-        :param engine: The engine route and login details
+        :param wallet_engine: The wallet_engine route and login details
         :return: a new instance of DAL class
-        :type engine: string
+        :type wallet_engine: string
         """
-        if not engine:
-            raise ValueError('The values specified in engine parameter has to be supported by SQLAlchemy')
-        self.engine = engine
-        db_engine = create_engine(engine,poolclass=NullPool)
+        if not wallet_engine:
+            raise ValueError('The values specified in wallet_engine parameter has to be supported by SQLAlchemy')
+        self.wallet_engine = wallet_engine
+        wallet_db_engine = create_engine(wallet_engine,poolclass=NullPool)
 
-        db_session = sessionmaker(bind=db_engine)
-        self.session = db_session()
-        logging.info("Opening connection")
+        wallet_db_session = sessionmaker(bind=wallet_db_engine)
+        self.wallet_session = wallet_db_session()
+        logging.info("Opening connection to Advarra Database")
+
+        if not op_engine:
+            raise ValueError('The values specified in op_engine parameter has to be supported by SQLAlchemy')
+        self.op_engine = op_engine
+        op_db_engine = create_engine(op_engine,poolclass=NullPool)
+
+        op_db_session = sessionmaker(bind=op_db_engine)
+        self.op_session = op_db_session()
+        logging.info("Opening connection to On Premise Database")
 
     def get_protocols(self):
         """
@@ -30,17 +39,17 @@ class DataProviderService:
                             :return: The protocol_no.
                             """
         try:
-            all_protocols = self.session.query(Protocols.protocol_no, Protocols.title).all()
+            all_protocols = self.wallet_session.query(Protocols.protocol_no, Protocols.title).all()
             return all_protocols
         except:
-            self.session.rollback()
+            self.wallet_session.rollback()
             raise
 
 
     def log_details(self, user_name, ip_address, call_time, validated,op_type):
         """
 
-               Creates and saves logFiles to the database.
+               Creates and saves logFiles to the on premise database.
 
                :param user_name: user name recorded from the request
                :param ip_address: ip address of the user
@@ -56,11 +65,11 @@ class DataProviderService:
                 validated=validated,
                 op_type=op_type
             )
-            self.session.add(new_log_detail)
-            self.session.commit()
+            self.op_session.add(new_log_detail)
+            self.op_session.commit()
             return new_log_detail.log_details_id
         except:
-            self.session.rollback()
+            self.op_session.rollback()
             raise
 
     def get_covid_protocols(self):
@@ -70,22 +79,22 @@ class DataProviderService:
         :return: The protocol_no.
         """
         try:
-            covid_protocols = self.session.query(DiseaseDiagnosis.protocol_id).filter("U07.1" == DiseaseDiagnosis.code).all()
+            covid_protocols = self.wallet_session.query(DiseaseDiagnosis.protocol_id).filter("U07.1" == DiseaseDiagnosis.code).all()
             covid_protocols_result = {}
             for x in covid_protocols:
                 if x[0] not in covid_protocols_result:
                     covid_protocols_result[x[0]] = {}
-                protocol = self.session.query(Protocols).filter(x[0]  == Protocols.protocol_id).first()
+                protocol = self.wallet_session.query(Protocols).filter(x[0]  == Protocols.protocol_id).first()
                 if protocol:
                     covid_protocols_result[x[0]]["protocol_no"] = protocol.protocol_no
                     covid_protocols_result[x[0]]["phase"] = protocol.phase
                     covid_protocols_result[x[0]]["nct_number"] = protocol.nct_number
                     covid_protocols_result[x[0]]["title"] = protocol.title
-                protocol_status = self.session.query(ProtocolStatus).filter(x[0]  == ProtocolStatus.protocol_id).first()
+                protocol_status = self.wallet_session.query(ProtocolStatus).filter(x[0]  == ProtocolStatus.protocol_id).first()
                 if protocol_status:
                     covid_protocols_result[x[0]]["status"] = protocol_status.status
                     covid_protocols_result[x[0]]["irb_no"] = protocol_status.irb_no
-                accrual_summary = self.session.query(ProtocolAccrual).filter(x[0] == ProtocolAccrual.protocol_id).first()
+                accrual_summary = self.wallet_session.query(ProtocolAccrual).filter(x[0] == ProtocolAccrual.protocol_id).first()
                 if accrual_summary:
                     covid_protocols_result[x[0]]["accruals"] = {}
                     covid_protocols_result[x[0]]["accruals"]["consented_count"] = accrual_summary.consented_count
@@ -99,10 +108,14 @@ class DataProviderService:
                     covid_protocols_result[x[0]]["accruals"]["not_eligible_count"] = accrual_summary.not_eligible_count
             return covid_protocols_result
         except:
-            self.session.rollback()
+            self.wallet_session.rollback()
             raise
 
     def close_connection(self):
-        if self.session is not None:
-            self.session.close()
-            logging.info("Closing connection")
+        if self.wallet_session is not None:
+            self.wallet_session.close()
+            logging.info("Closing connection to Advarra database")
+
+        if self.op_session is not None:
+            self.op_session.close()
+            logging.info("Closing connection to On-Premise database")
